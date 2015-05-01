138e3d15822fa6a985f57a928cd15534846bf555
Jason Cidras <jcidras@me.com>
2015-05-01 11:50:10 -0500

    First Commit
    Simple Angular User Management System
    Using Web Service, responsive css

---
+++ js/controllers.js
@@ -0,0 +1,58 @@
+﻿var controllers = angular.module('UserControllers', []);
+
+controllers.controller('MainCtrl',
+	['$scope', '$http',
+		function ($scope, $http) {
+			$http.get('http://localhost/api/GetUsers')
+			.success(function (response) {
+				$scope.users = response;
+			});
+		}
+	]
+);
+
+controllers.controller('CreateCtrl',
+	['$scope', '$http', '$location',
+		function ($scope, $http, $location) {
+		    $scope.save = function () {                
+				var data = {
+					FirstName: $scope.user.FirstName,
+					LastName: $scope.user.LastName,
+					Email: $scope.user.Email,
+					Age: $scope.user.Age
+				};
+				$http.post('http://localhost/api/AddUser/', data)
+				.success(function (response) {
+					$location.path("/");
+				});
+				$scope.user.FirstName = "";
+				$scope.user.LastName = "";
+				$scope.user.Email = "";
+				$scope.user.Age = "";
+			};
+		}
+	]
+);
+
+controllers.controller('EditCtrl',
+	['$scope', '$http', '$routeParams', '$location',
+		function ($scope, $http, $routeParams, $location) {		
+			$http.get('http://localhost/api/GetUser/' + $routeParams.Id)
+			.success(function (response) {
+				$scope.user = response;
+			})
+			.error(function (response) {
+				alert(response);
+			});	  
+
+			$scope.delete = function (id) {
+				if (confirm("Do you wish to delete this user?")) {
+					$http.delete('http://localhost/api/DeleteUser/' + id)
+					.success(function (response) {
+						$location.path("/");
+					});
+				}
+			};
+		}
+	]
+);
---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('app', ['ngRoute', 'UserControllers']);
+
+app.config(
+	['$routeProvider',
+		function ($routeProvider) {
+		    $routeProvider
+			.when('/list', {
+			    templateUrl: 'templates/list.html',
+			    controller: 'MainCtrl'
+			})
+			.when('/new', {
+			    templateUrl: 'templates/edit.html',
+			    controller: 'CreateCtrl'
+			})
+			.when('/edit/:Id', {
+			    templateUrl: 'templates/edit.html',
+			    controller: 'EditCtrl'
+			})
+			.otherwise({
+			    redirectTo: '/list'
+			});
+		}
+	]
+);
